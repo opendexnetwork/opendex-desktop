30b7b32a2d5be0232eca207062bbbc219afb552a
kilrau <56045404+BitcoinOG@users.noreply.github.com>
2021-02-22 09:36:16 +0100

    fix: discord+docs links

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
-export const OPENDEX_DISCORD_URL = "https://discord.gg/RnXFHpn";
+export const OPENDEX_DISCORD_URL = "https://discord.gg/aS5RMchDrU";
 
 export const OPENDEX_DOCKER_DOCS_URL =
-  "https://opendex.network/docs/start-earning/market-maker-guide";
+  "https://opendex.network/docs";
 
 export const OPENDEX_DOCKER_LOCAL_MAINNET_URL = "https://localhost:8889";
 export const OPENDEX_DOCKER_LOCAL_TESTNET_URL = "https://localhost:18889";
